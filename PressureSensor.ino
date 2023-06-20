a7ef24126f77ac9e92d4a3174879b90654ecff1a
Ian Lansdowne <idlansdowne@gmail.com>
2023-06-19 22:27:37 -0500

    Added USB serial functionality

--- PressureSensor.ino
+++ PressureSensor.ino
@@ -1,8 +1,33 @@
 #include <Adafruit_MPRLS.h>
 #include <BluetoothSerial.h>
 
+constexpr double PSI_to_KPA{PSI_to_HPA / 10.0};
+constexpr double KPA_to_PSI{10.0 / PSI_to_HPA};
+
 BluetoothSerial SerialBT;
-Adafruit_MPRLS mpr = Adafruit_MPRLS{-1, -1, 0, 25, 10, 90, 1};
+Adafruit_MPRLS mpr = Adafruit_MPRLS{-1, -1, 0, 25, 10, 90, PSI_to_KPA};  // psi
+
+// Packet structure for serial communication
+struct {
+  uint8_t startingBit{0xFA};
+  float pressure;
+  uint16_t crc;
+} __attribute__((packed)) packet;
+
+uint16_t calculateCRC() {
+  constexpr size_t length{sizeof(packet) - sizeof(packet.startingBit) -
+                          sizeof(packet.crc)};
+  uint8_t *data{(uint8_t *)&packet + sizeof(packet.startingBit)};
+  uint16_t crc = 0;
+  for (size_t i = 0; i < length; i++) {
+    crc = (uint8_t)(crc >> 8) | (crc << 8);
+    crc ^= data[i];
+    crc ^= (uint8_t)(crc & 0xff) >> 4;
+    crc ^= crc << 12;
+    crc ^= (crc & 0x00ff) << 5;
+  }
+  return crc;
+}
 
 void setup() {
   SerialBT.begin("PressureSensor");
@@ -10,9 +35,16 @@ void setup() {
     delay(1000);
     return;
   }
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
 }
 
 void loop() {
-  SerialBT.println(mpr.readPressure());  // pressure in psi
-  delay(5);
+  // Serial.printf("%.2f V\n", analogReadMilliVolts(A13) * 0.002f);
+  packet.pressure = mpr.readPressure();
+  packet.crc = calculateCRC();
+  Serial.write((uint8_t *)&packet, sizeof(packet));
+
+  SerialBT.println((packet.pressure - 100.0) * KPA_to_PSI);  // pressure in psi
+  delay(20);
 }
