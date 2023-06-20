fd0d79dd61bc788c2ee102975a1840d175bd2207
Ian Lansdowne <idlansdowne@gmail.com>
2023-06-20 16:24:55 -0500

    Swapped endianness to fix issues reading checksum; works on roborio 🎉

--- PressureSensor.ino
+++ PressureSensor.ino
@@ -10,27 +10,29 @@ Adafruit_MPRLS mpr = Adafruit_MPRLS{-1, -1, 0, 25, 10, 90, PSI_to_KPA};  // psi
 // Packet structure for serial communication
 struct {
   uint8_t startingBit{0xFA};
-  float pressure;
+  struct {
+    float pressure;
+  } __attribute__((packed)) data;
   uint16_t crc;
 } __attribute__((packed)) packet;
 
 void calculateCRC() {
-  constexpr size_t length{sizeof(packet) - sizeof(packet.startingBit) -
-                          sizeof(packet.crc)};
-  uint8_t *data{(uint8_t *)&packet + sizeof(packet.startingBit)};
-  uint16_t crc = 0;
-  for (size_t i = 0; i < length; i++) {
+  constexpr size_t length{sizeof(packet.data)};
+  uint8_t *data{(uint8_t *)&packet.data};
+  uint16_t crc{0};
+  for (size_t i{0}; i < length; i++) {
     crc = (uint8_t)(crc >> 8) | (crc << 8);
     crc ^= data[i];
     crc ^= (uint8_t)(crc & 0xff) >> 4;
     crc ^= crc << 12;
     crc ^= (crc & 0x00ff) << 5;
   }
-  packet.crc = crc;
+  // swap from little endian to big endian
+  packet.crc = __builtin_bswap16(crc);
 }
 
 void setup() {
-  SerialBT.begin("PressureSensor");
+  // SerialBT.begin("PressureSensor");
   Serial.begin(115200);
   if (!mpr.begin()) {
     delay(1000);
@@ -42,10 +44,13 @@ void setup() {
 
 void loop() {
   // Serial.Serial.printf("%.2f V\n", analogReadMilliVolts(A13) * 0.002f);
-  packet.pressure = mpr.readPressure();
+  packet.data.pressure = mpr.readPressure();
   calculateCRC();
   Serial.write((uint8_t *)&packet, sizeof(packet));
 
-  SerialBT.println((packet.pressure - 100.0) * KPA_to_PSI);  // pressure in psi
-  delay(20);
+  // Serial.printf("%d\n", calculateCRC2());
+
+  // SerialBT.println((packet.pressure - 100.0) * KPA_to_PSI);  // pressure
+  // in psi
+  delay(5);
 }
